2c9bf35fb057816b2da21b4636f7e3dd19645697
wiwofone <apeirion@gmail.com>
2015-08-11 17:35:51 +0200

    Design the split view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'spotify'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,66 @@
  * Controller of the posterboyApp
  */
 angular.module('posterboyApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function (MusixService, Spotify) {
 
     var ctrl = this;
 
     ctrl.things = ['lol', 'boll', 'hej'];
 
-    ctrl.generatePoster = function () {
-      // DO LOGIC HERE
-      console.log('lol!');
+    // Default poster vars
+    ctrl.poster = {
+      lyrics: ['I’m living the future so the present is my past'], // i'm thinking an array of lyrics lines?
+      quoteIndex: 0, // index of array to pick quote line
+      artist: 'Kanye West',
+      track: 'Monster',
+      artistImageUrl: 'http://uptownmagazine.com/files/2015/01/People-Kanye_West.jpg'
+    };
+
+    // Quotes poster controls
+    ctrl.quoteNext = function () {
+      if((ctrl.poster.quoteIndex + 1) < ctrl.poster.lyrics.length) {
+        ctrl.poster.quoteIndex = ctrl.poster.quoteIndex + 1;
+        console.log(ctrl.poster.quoteIndex);
+      }
+    };
+
+    ctrl.quotePrev = function () {
+      if((ctrl.poster.quoteIndex - 1) >= 0) {
+        ctrl.poster.quoteIndex = ctrl.poster.quoteIndex - 1;
+      }
+    };
+
+    ctrl.getInfo = function (trackId) {
+
+      /* Get track ID from URI or link (or none) */
+      trackId = trackId.split(':').slice(-1)[0].split('/').slice(-1)[0];
+      console.log(trackId);
+
+      /* Get track artist and name */
+      Spotify.getTrack(trackId).then(function(response) {
+        console.log(response);
+        ctrl.poster.track = response.name;
+        ctrl.poster.artist = response.artists[0].name;
+        ctrl.poster.artistImageUrl = response.album.images[0].url;
+      });
+
+      /* Get lyrics */
+      MusixService.getMusixId(trackId).then(function(response) {
+        if(response && response.data && response.data.response && response.data.response.songs) {
+          var musixId = response.data.response.songs[0].foreign_ids[0].foreign_id.split(':').slice(-1)[0];
+          MusixService.getLyrics(musixId).then(function(response) {
+            if(response && response.data && response.data.message && response.data.message.body
+              && response.data.message.body.lyrics) {
+                ctrl.poster.lyrics = response.data.message.body.lyrics.lyrics_body.match(/[^\r\n]+/g);
+                console.log(ctrl.poster.lyrics);
+              } else {
+                console.log('error'); // TODO: BETTER ERROR HANDLING
+              }
+          });
+        }
+      });
+
+
     };
 
   });
